5bf37814087ff853f0a8ebe09e74842f0039e336
Kubilay Kaya <kubilay.kaya@siemens.com>
2022-12-07 15:30:48 +0300

    Some updates on test document

--- MotorSürücü/MotorS_r_c__Ilk/MotorS_r_c__Ilk.ino
+++ MotorSürücü/MotorS_r_c__Ilk/MotorS_r_c__Ilk.ino
@@ -2,6 +2,7 @@
 int Enable_B = 11;
 int inputB1 = 6;
 int inputB2 = 9;
+int inputRead = A1;
 int i;
 int k;
 void setup()
@@ -11,24 +12,10 @@ Serial.begin(9600);
 pinMode(Enable_B, OUTPUT);
 pinMode(inputB1, OUTPUT);
 pinMode(inputB2, OUTPUT);
+pinMode(inputRead,INPUT);
 }
 void loop()
 {
  
-for (i=0; i<120; i++) {
-Serial.println(i);
-analogWrite(Enable_B,i);
-digitalWrite(inputB1,HIGH);
-digitalWrite(inputB2,LOW);
-delay(100);
-}
-
-for(k=120;k>0;k--){
-  analogWrite(Enable_B,k);
-  digitalWrite(inputB1,HIGH);
-  digitalWrite(inputB2,LOW);
-  delay(100);
-}
-
 
 }
